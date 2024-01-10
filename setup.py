09789ff7aac3a338d6f064eadcff7c98ec07ed34
KÛЙÐÅŊ <kundan9910558927@gmail.com>
2024-01-10 18:18:01 +0530

    file update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mcqgenrator',
+    version = '0.0.1',
+    author = 'KÛЙÐÅŊ',
+    author_email = 'kundan9910558927@gmail.com',
+    install_requires = ["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+    
+    )
